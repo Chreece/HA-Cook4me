6425d5244ce32952bca8946d523317ff98d726db
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2026-09-06 19:23:43 +0200

    Register Recipe Hub v12 websocket API

--- custom_components/cook4me/__init__.py
+++ custom_components/cook4me/__init__.py
@@ -18,6 +18,7 @@ from .websocket_v8 import async_register as async_register_websocket_v8
 from .websocket_v9 import async_register as async_register_websocket_v9
 from .websocket_v10 import async_register as async_register_websocket_v10
 from .websocket_v11 import async_register as async_register_websocket_v11
+from .websocket_v12 import async_register as async_register_websocket_v12
 
 SERVICE_SEND_RECIPE = "send_recipe"
 SERVICE_SEARCH_RECIPES = "search_recipes"
@@ -125,6 +126,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     async_register_websocket_v9(hass)
     async_register_websocket_v10(hass)
     async_register_websocket_v11(hass)
+    async_register_websocket_v12(hass)
     await async_register_panel(hass)
     return True
 
