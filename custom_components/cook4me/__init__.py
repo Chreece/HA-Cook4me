d4771c62ef63f70acebb3820835626387dd1c167
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2026-09-07 09:27:22 +0200

    Register nutrition WebSocket API

--- custom_components/cook4me/__init__.py
+++ custom_components/cook4me/__init__.py
@@ -28,6 +28,7 @@ from .websocket_v12 import async_register as async_register_websocket_v12
 from .websocket_v13 import async_register as async_register_websocket_v13
 from .websocket_v14 import async_register as async_register_websocket_v14
 from .websocket_v15 import async_register as async_register_websocket_v15
+from .websocket_v16 import async_register as async_register_websocket_v16
 
 SERVICE_SEND_RECIPE = "send_recipe"
 SERVICE_SEARCH_RECIPES = "search_recipes"
@@ -237,6 +238,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     async_register_websocket_v13(hass)
     async_register_websocket_v14(hass)
     async_register_websocket_v15(hass)
+    async_register_websocket_v16(hass)
     await async_register_panel(hass)
     return True
 
