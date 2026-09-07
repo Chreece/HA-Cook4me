23732bedb6e21c99b222537a8fb519a17fd050cd
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2026-09-07 15:15:42 +0200

    Activate Recipe Hub v33 freeze hotfix

--- custom_components/cook4me/panel.py
+++ custom_components/cook4me/panel.py
@@ -10,8 +10,8 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
 _URL_BASE = "/cook4me_static"
-_PANEL_ELEMENT = "cook4me-recipe-hub-panel-v32"
-_PANEL_MODULE = "cook4me-panel-v32.js"
+_PANEL_ELEMENT = "cook4me-recipe-hub-panel-v33"
+_PANEL_MODULE = "cook4me-panel-v33.js"
 
 
 async def async_register_panel(hass: HomeAssistant) -> None:
@@ -25,7 +25,7 @@ async def async_register_panel(hass: HomeAssistant) -> None:
         hass=hass,
         frontend_url_path=DOMAIN,
         webcomponent_name=_PANEL_ELEMENT,
-        module_url=f"{_URL_BASE}/{_PANEL_MODULE}?v=2026.9.7.11",
+        module_url=f"{_URL_BASE}/{_PANEL_MODULE}?v=2026.9.7.12",
         sidebar_title="Cook4Me",
         sidebar_icon="mdi:pot-steam",
         embed_iframe=False,
