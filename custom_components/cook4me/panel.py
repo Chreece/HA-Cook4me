3248b92810963390a0dcce05930d3ce1c7143d54
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2026-09-08 09:52:29 +0200

    Activate Recipe Hub v51 lazy startup

--- custom_components/cook4me/panel.py
+++ custom_components/cook4me/panel.py
@@ -17,8 +17,8 @@ from .websocket_v25 import async_register as async_register_websocket_v25
 from .websocket_v26 import async_register as async_register_websocket_v26
 
 _URL_BASE = "/cook4me_static"
-_PANEL_ELEMENT = "cook4me-recipe-hub-panel-v50"
-_PANEL_MODULE = "cook4me-panel-v50.js"
+_PANEL_ELEMENT = "cook4me-recipe-hub-panel-v51"
+_PANEL_MODULE = "cook4me-panel-v51.js"
 _V20_REGISTERED = "websocket_v20_registered"
 _V21_REGISTERED = "websocket_v21_registered"
 _V22_REGISTERED = "websocket_v22_registered"
@@ -61,7 +61,7 @@ async def async_register_panel(hass: HomeAssistant) -> None:
         hass=hass,
         frontend_url_path=DOMAIN,
         webcomponent_name=_PANEL_ELEMENT,
-        module_url=f"{_URL_BASE}/{_PANEL_MODULE}?v=2026.9.8.5",
+        module_url=f"{_URL_BASE}/{_PANEL_MODULE}?v=2026.9.8.6",
         sidebar_title="Cook4Me",
         sidebar_icon="mdi:pot-steam",
         embed_iframe=False,
