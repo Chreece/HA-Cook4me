03499b87a10255fc171a5cdc9ab3d8b3b6c7b4ce
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2026-09-08 08:35:29 +0200

    Route remaining online reads through daily cache

---
+++ custom_components/cook4me/frontend/cook4me-panel-v50.js
@@ -0,0 +1,20 @@
+import "./cook4me-panel-v49.js";
+
+const BasePanel=customElements.get("cook4me-recipe-hub-panel-v49");
+
+class Cook4MeRecipeHubPanelV50 extends BasePanel{
+  async _api(type,data={}){
+    const mapped={
+      "cook4me/v21/currency_state":"cook4me/v24/currency_state",
+      "cook4me/v21/currency_set":"cook4me/v24/currency_set",
+      "cook4me/recipe_detail":"cook4me/v24/recipe_detail",
+      "cook4me/v7/recipe_detail":"cook4me/v24/recipe_detail",
+      "cook4me/v9/recipe_detail":"cook4me/v24/recipe_detail",
+    }[type]||type;
+    const payload={...(data||{})};
+    delete payload.refresh;
+    return super._api(mapped,payload);
+  }
+}
+
+customElements.define("cook4me-recipe-hub-panel-v50",Cook4MeRecipeHubPanelV50);
